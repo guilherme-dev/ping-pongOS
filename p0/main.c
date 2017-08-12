f0fdb847adfa7bf107d8e8bdd8a26601a4ec2594
Guilherme Santos <ggs12@inf.ufpr.br>
2017-08-12 18:15:01 -0300

    makefile, main.c e queue.c criados

---
+++ p0/main.c
@@ -0,0 +1,11 @@
+// Arquivo para testes durante desenvolvimento
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+#define N 100
+
+int main (int argc, char **argv, char **envp)
+{
+	return 0;
+}
